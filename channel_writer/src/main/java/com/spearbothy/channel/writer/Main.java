2b049a077bf7b8c14dcb6f419a029a42d4b8d4a2
mahao <zziamahao@163.com>
2017-08-11 15:18:23 +0800

    添加签名验证

--- channel_writer/src/main/java/com/spearbothy/channel/writer/Main.java
+++ channel_writer/src/main/java/com/spearbothy/channel/writer/Main.java
@@ -2,14 +2,14 @@ package com.spearbothy.channel.writer;
 
 
 import com.spearbothy.channel.common.ApkSignBlockUtil;
-import com.spearbothy.channel.common.SignatureNotFoundException;
 import com.spearbothy.channel.common.log.Log;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by mahao on 17-8-3.
@@ -22,6 +22,8 @@ public class Main {
         Log.i("读取渠道文件");
         List<String> channelList = FileUtil.readChannel("channel_writer/channel.txt");
         List<String> errorList = new ArrayList<>();
+        Map<String, String> v2ErrorList = new HashMap<>();
+        Map<String, String> v1ErrorList = new HashMap<>();
         Log.i("\t渠道列表：" + channelList);
         File apkFile = new File("channel_writer/app-release.apk");
         Log.d("获取apk path:" + apkFile.exists());
@@ -45,11 +47,12 @@ public class Main {
             try {
                 Log.i("\t写入渠道信息：id=" + ApkSignBlockUtil.APK_SIGNATURE_CHANNEL_ID + ",channel=" + channel);
                 ApkSignBlockWriter.put(channelFile, ApkSignBlockUtil.APK_SIGNATURE_CHANNEL_ID, channel);
-            } catch (SignatureNotFoundException e) {
-                errorList.add(channel);
-                Log.e(channel + "  fail !!!");
-                Log.d(e.getMessage());
-            } catch (IOException e) {
+
+                if (!verifyApk(channel, channelFile, v1ErrorList, v2ErrorList)) {
+                    throw new Exception("签名验证失败！！！");
+                }
+            } catch (Exception e) {
+                channelFile.delete();
                 errorList.add(channel);
                 Log.e(channel + "  fail !!!");
                 Log.d(e.getMessage());
@@ -59,5 +62,34 @@ public class Main {
         }
         Log.i("-----------------------------------------");
         Log.i("渠道注入失败列表：" + errorList);
+        Log.i("v1签名验证失败列表：");
+        for (Map.Entry<String, String> entry : v1ErrorList.entrySet()) {
+            Log.e("\t" + entry.getKey() + "---" + entry.getValue());
+        }
+        Log.i("v2签名验证失败列表：");
+        for (Map.Entry<String, String> entry : v2ErrorList.entrySet()) {
+            Log.e("\t" + entry.getKey() + "---" + entry.getValue());
+        }
+    }
+
+    public static boolean verifyApk(String channel, File file, Map<String, String> v1, Map<String, String> v2) {
+        boolean success = true;
+        try {
+            VerifyUtil.verifyV2Signature(file);
+            Log.i("\tv2签名验证成功:" + channel);
+        } catch (Exception e) {
+            v2.put(channel, e.getMessage());
+            success = false;
+            Log.i("\tv2签名验证失败:" + channel);
+        }
+        try {
+            VerifyUtil.verifyV1Signature(file);
+            Log.i("\tv1签名验证成功:" + channel);
+        } catch (Exception e) {
+            v1.put(channel, e.getMessage());
+            success = false;
+            Log.i("\tv1签名验证失败:" + channel);
+        }
+        return success;
     }
 }
